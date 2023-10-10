dd1ae48b8bdc115d11f83e5fb11e8d40fce2f4d6
PetrachkovVasily <vasyapt04@gmail.com>
2023-10-10 16:55:56 +0300

    Update input.js

--- PIIS/Lab_6/input.js
+++ PIIS/Lab_6/input.js
@@ -9,8 +9,9 @@ function moveElement(event) {
     let selected = event.target;
     selected.style.position = 'absolute'; 
 
-    document.addEventListener('touchmove', moveAt)
-    document.addEventListener('touchend', stopMoving)
+    document.addEventListener('touchmove', moveAt);
+    document.addEventListener('touchend', stopMoving);
+    document.addEventListener('touchstart', twoTouches);
 
     function moveAt(event) {
         console.log('sdfhj')
@@ -23,5 +24,11 @@ function moveElement(event) {
         topP = selected.style.top;
         document.removeEventListener('touchmove', moveAt);
         document.removeEventListener('touchend', stopMoving);
+    }
+
+    function twoTouches(event) {
+        if (event.touches.length > 1){
+            stopMoving(event);
+        }
     }
 }
