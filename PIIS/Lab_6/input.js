82ba0fbbfb572645aa8d908372de95385e8ed865
PetrachkovVasily <vasyapt04@gmail.com>
2023-10-10 13:44:38 +0300

    Update input.js

--- PIIS/Lab_6/input.js
+++ PIIS/Lab_6/input.js
@@ -16,7 +16,7 @@ function moveElement(event) {
         selected.style.left = pageX - selected.offsetWidth / 2 + 'px';
         selected.style.top = pageY - selected.offsetHeight / 2 + 'px';
         if (event.touches.length == 2) {
-            alert('j')
+           
             document.removeEventListener('touchmove', onFingerMove);
             selected.style.left = leftP;
             selected.style.top = topP;
